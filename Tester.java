ca9daac5d04235ddf12ce586a37c78269fdffad6
Jonathan Wu <jwu20@stuy.edu>
2020-10-29 17:06:23 -0400

    Refined code

--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
   public static void main(String[] args){
     Point pointOne = new Point(4,5);
-    Point pointTwo = new Point(5,11);
-    System.out.println(pointOne.distanceTo(pointTwo));
+    Point pointTwo = new Point(4,5);
+    System.out.println("The distance is " + pointOne.distanceTo(pointTwo));
+    System.out.println("Are these two points equal? " + pointOne.equals(pointTwo));
   }
 }
